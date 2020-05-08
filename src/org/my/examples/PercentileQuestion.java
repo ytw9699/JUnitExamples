434f4e9c3176ec3d37956d76d24a4360637f23fb
home_champ <ytw9699@naver.com>
2020-05-09 00:52:59 +0900

    2번째 테스트

---
+++ src/org/my/examples/PercentileQuestion.java
@@ -0,0 +1,12 @@
+package org.my.examples;
+
+public class PercentileQuestion extends Question {
+   public PercentileQuestion(int id, String text, String[] answerChoices) {
+      super(id, text, answerChoices);
+   }
+
+   @Override
+   public boolean match(int expected, int actual) {
+      return expected <= actual;
+   }
+}
