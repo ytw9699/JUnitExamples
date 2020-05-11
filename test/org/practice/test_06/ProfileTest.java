daed5badd930630a875ec8c3ce0b001cda31f64c
home_champ <ytw9699@naver.com>
2020-05-11 21:34:39 +0900

    src 업데이트

---
+++ test/org/practice/test_06/ProfileTest.java
@@ -0,0 +1,25 @@
+/***
+ * Excerpted from "Pragmatic Unit Testing in Java with JUnit",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/utj2 for more book information.
+***/
+package org.practice.test_06;
+
+import org.junit.*;
+import org.practice.examples_06.*;
+
+public class ProfileTest {
+
+   @Test
+   public void test() {
+      Profile profile = new Profile("Bull Hockey, Inc.");
+      Question question = new BooleanQuestion(1, "Got bonuses?");
+      Criteria criteria = new Criteria();
+      Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+      Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+      criteria.add(criterion);
+   }
+}
